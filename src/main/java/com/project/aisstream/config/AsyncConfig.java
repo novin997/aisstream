8c6f66c4ad92d0d1478f9c33e82557d616084147
Novin <cocolupiayi@gmail.com>
2023-02-15 17:32:00 +0800

    Add async executor

---
+++ src/main/java/com/project/aisstream/config/AsyncConfig.java
@@ -0,0 +1,19 @@
+package com.project.aisstream.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean("threadPoolTaskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        return executor;
+    }
+}
